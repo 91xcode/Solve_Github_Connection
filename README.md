eb6fb556018c3153c3db6c32255fb8b3fdb6c387
liubing1 <liubing1@conew.com>
2022-02-15 14:49:33 +0800

    add

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+expect my.sh
---
+++ my.sh
@@ -0,0 +1,15 @@
+#autorm.sh#expect的安装路径
+#/usr/bin/expect这个路径安装完expect之后 which expect可以查看
+#!/usr/bin/expect -f
+
+#设置超时时间 
+set timeout 3
+#设置你的Mac用户密码
+set password xxxxxx
+#传递交互指令
+spawn sudo sh edit-host.sh
+#根据输出传递数据,在这里是等待密码提示显示
+expect "*asswor*" 
+send "$password\r"
+#保持在远端  
+interact
